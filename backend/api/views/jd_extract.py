26073acaddc71033ea06a8188aa9c65821679646
awais <bilalandme88@gmail.com>
2026-04-07 14:29:42 +0500

    init

--- backend/api/views/jd_extract.py
+++ backend/api/views/jd_extract.py
@@ -1,36 +1,44 @@
 from ninja import Router, Form
-from google import genai
-from google.genai import types
+from ollama import AsyncClient
 
 
 router = Router()
 
-@router.post("/jd-extract-gemini")
-async def format_job_description_gemini(request, raw_text: str = Form(...)):
+
+@router.post("/jd-format-ollama")
+async def format_job_description_ollama(request, raw_text: str = Form(...)):
     
-    client = genai.Client(api_key="AIzaSyDYU5WWmvU5Bht3kOq_7EROhHWUdX87Gmo")
+    client = AsyncClient(host="http://192.168.1.184:11434")
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a data extractor. Extract only: Title | Company. Do not say anything else. Example: Software Engineer | Google"
+        },
+        {
+            "role": "user",
+            "content": f"Extract from this text:\n{raw_text}"
+        }
+    ]
+
+    response = await client.chat(
+        model='llama3', 
+        messages=messages,
+        options={'temperature': 0}
+    )
     
-    async with client.aio as async_client:
-        response = await async_client.models.generate_content(
-            model='gemini-3.1-flash',
-            contents=[
-                "Extract only Title and Company Name. Use Exact Format: Title | Company",
-                raw_text
-            ],
-            config=types.GenerateContentConfig(temperature=0)
-        )
-    
+    ai_text = response['message']['content']
     try:
-        title, company = response.text.split("|", 1)
+        title, company = ai_text.split("|", 1)
         extracted_title = title.strip()
         extracted_company = company.strip()
-    except Exception:
-        extracted_title = "Not Found"
-        extracted_company = "Not Found"
+    except ValueError:
+        extracted_title = "Check original text"
+        extracted_company = "Check original text"
 
     return {
         "status": "success",
         "title": extracted_title,
         "company": extracted_company,
-        "full_response": response.text
+        "full_response": ai_text
     }
+
